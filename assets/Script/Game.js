50663d60f62ad8ee99c4dee847ff04632938bebe
DuongHoangHiep <duong.hoanghiepp@gmail.com>
2023-03-19 17:54:43 +0700

    update game

--- assets/Script/Game.js
+++ assets/Script/Game.js
@@ -38,13 +38,26 @@ cc.Class({
   },
 
   handleInitGame() {
-    this.hanldeStartPositionOfBirds(this.Bird1, -60, 72, 1, -0.08, 0.08, );
+    this.hanldeStartPositionOfBirds(this.Bird1, -60, 72, 1, -0.08, 0.08, false);
+    this.hanldeStartPositionOfBirds(this.Bird2, -85, 72, 1, -0.08, 0.08, true);
+    this.hanldeStartPositionOfBirds(this.Bird3, -110, 72, 1, -0.08, 0.08, false);
+    this.hanldeStartPositionOfBirds(this.Bird4, -140, 72, 1, -0.08, 0.08, true);
+    // this.hanldeStartPositionOfBirds(this.Bird5, -60, 72, 1, -0.08, 0.08);
+    // this.hanldeStartPositionOfBirds(this.Bird6, -60, 72, 1, -0.08, 0.08);
+    // this.hanldeStartPositionOfBirds(this.Bird7, -60, 72, 1, -0.08, 0.08);
+    // this.hanldeStartPositionOfBirds(this.Bird8, -60, 72, 1, -0.08, 0.08);
+    // this.hanldeStartPositionOfBirds(this.Bird9, -60, 72, 1, -0.08, 0.08);
+    // this.hanldeStartPositionOfBirds(this.Bird10, -60, 72, 1, -0.08, 0.08);
+    // this.hanldeStartPositionOfBirds(this.Bird11, -60, 72, 1, -0.08, 0.08);
+    // this.hanldeStartPositionOfBirds(this.Bird12, -60, 72, 1, -0.08, 0.08);
   },
 
-  hanldeStartPositionOfBirds(node, posX, posY, duration, rotateX, rotateY, animName, loopAnim) {
+  hanldeStartPositionOfBirds(node, posX, posY, duration, rotateX, rotateY, isRotate) {
     this.handleSetPosition(node, posX, posY, duration);
-    this.handleSetRotateBird(node, rotateX, rotateY, duration);
-    this.handleSetAnim(node, "grounding", "idle", false);
+    this.handleSetRotateBird(node, rotateX, rotateY, duration, isRotate);
+    this.scheduleOnce(() => {
+      this.handleSetAnim(node, "grounding", "idle", false);
+    }, 1 );
   },
 
   handleSetPosition(node, posX, posY, duration) {
@@ -53,19 +66,26 @@ cc.Class({
     node.runAction(moveAction);
   },
 
-  handleSetRotateBird(node, rotateX, rotateY, duration) {
+  handleSetRotateBird(node, rotateX, rotateY, duration, activeRotate = false) {
+
     const delayAction = cc.delayTime(duration);
     const sequenceAction = cc.sequence(delayAction, cc.scaleTo(0, rotateX, rotateY));
-    node.runAction(sequenceAction);
+
+    if(activeRotate) {
+      console.log("check")
+      node.runAction(cc.sequence(cc.delayTime(0), cc.scaleTo(0, rotateX, rotateY)));
+    } else {
+      node.runAction(sequenceAction);
+    }
   },
 
   handleSetAnim(node, groundAnim, idleAnim, loop = false) {
     const skeletonComp = node.getComponent("sp.Skeleton");
     skeletonComp.setAnimation(0, groundAnim, loop);
 
-    const delayAction = cc.delayTime(1);
-    const sequenceAction = cc.sequence(delayAction, skeletonComp.setAnimation(0, idleAnim, loop));
-    node.runAction(sequenceAction);
+    this.scheduleOnce(() => {
+      skeletonComp.setAnimation(0, idleAnim, true);
+    }, 1 );
   }
 
   // update (dt) {},
